73e32087a0734e8736d503eb31a914b813ad12bb
shabbir1993 <syed_shabbir_haider@hotmail.com>
2016-09-20 23:56:26 +0500

    Fixed database name, finalized all requirements

--- RaceDay/config/application.rb
+++ RaceDay/config/application.rb
@@ -11,6 +11,8 @@ module RaceDay
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #bootstraps mongoid within applications -- like rails console
+    Mongoid.load!('./config/mongoid.yml')
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -19,7 +21,7 @@ module RaceDay
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    config.generators {|g| g.orm :mongoid}
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
